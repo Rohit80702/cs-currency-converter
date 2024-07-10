e74788996920c4a42749e1df16b1cc9bcb6e206c
Rohit80702 <rsharma8072002@gmail.com>
2024-07-10 16:21:36 +0530

    initial commit

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ import Freecurrencyapi from '@everapi/freecurrencyapi-js';
 const freecurrencyapi = new Freecurrencyapi('fca_live_pBl32eUKIikMZk0VahityxmIFmKtfmcjCWDIE7RI');
 
 export async function convertCurrency(fromCurrency, toCurrency, units) {
-    const result = await freecurrencyapi.latest({
+    const res = await freecurrencyapi.latest({
         base_currency: fromCurrency,
         currencies: toCurrency
     });
-    const multiplier = result.data[toCurrency];
+    const multiplier = res.data[toCurrency];
     return units * multiplier;
 }
